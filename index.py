cf44a851dbbabf2d90966c86311f6b19a39310a9
unknown <brightokusi@gmail.com>
2024-03-14 20:53:16 +0000

    Retrieving the data from the API and uploading to the raw s3 bucket

--- index.py
+++ index.py
@@ -7,8 +7,8 @@ import json
 import requests
 import psycopg2
 
-from utils.help import create_bucket
-
+from utils.help import create_bucket, retrieve_api_data_and_upload_to_s3
+from utils.constants import raw_file_path
 
 config = configparser.ConfigParser()
 config.read('.env')
@@ -22,8 +22,14 @@ transformed_bucket_name = config['AWS']['transformed_bucket_name']
 region = config['AWS']['region']
 role = config['AWS']['role']
 
+['API']
+api_key = config['API']['api_key']
 
 
 #=====create an s3 bucket
 create_bucket(access_key, secret_key, raw_bucket_name, region)
+
+
+#========= Load json file to bucket
+retrieve_api_data_and_upload_to_s3(access_key, secret_key, api_key, raw_bucket_name, raw_file_path)
 
--- utils/helper.py
+++ utils/helper.py
@@ -54,3 +54,40 @@ def create_bucket(access_key, secret_key, bucket_name, region):
     except Exception as e:
         print(f"An error occurred:", e)
 
+
+def retrieve_api_data_and_upload_to_s3(access_key, secret_key, api_key, raw_bucket_name, raw_file_path):
+    try:
+        url = "https://jsearch.p.rapidapi.com/search"
+        merged_json = {}  # Initialize an empty dictionary to store merged data
+
+        query_strings = [
+            {"query": "Data Engineer or Data Analyst in USA", "page": "1", "num_pages": "10", "date_posted": "today"},
+            {"query": "Data Engineer or Data Analyst in Canada", "page": "1", "num_pages": "10", "date_posted": "today"},
+            {"query": "Data Engineer or Data Analyst in UK", "page": "1", "num_pages": "10", "date_posted": "today"}
+        ]
+        
+        # Dictionary to store data for each country
+        country_data = {}
+
+        for querystring in query_strings:
+            headers = {"X-RapidAPI-Key": api_key, "X-RapidAPI-Host": 'jsearch.p.rapidapi.com'}
+            response = requests.get(url, headers=headers, params=querystring)
+            
+            if response.status_code == 200:
+                data = response.json()  # Get the JSON response as a dictionary
+                country_name = querystring['query'].split()[-1]  # Extract country name from query
+                
+                # Store data for each country separately
+                country_data[country_name] = data
+                
+        # Convert country-wise data to JSON strings and upload to S3
+        for country, data in country_data.items():
+            json_data = json.dumps(data)
+            
+            # Upload JSON data to S3 bucket with country-specific file paths
+            s3 = boto3.client('s3', aws_access_key_id=access_key, aws_secret_access_key=secret_key)
+            s3.put_object(Bucket=raw_bucket_name, Key=f"{raw_file_path}_{country}.json", Body=json_data)
+            print(f'API data retrieved as {raw_file_path}_{country}.json and loaded into {raw_bucket_name}')
+        
+    except Exception as e:
+        print('There was an error: ', e)
