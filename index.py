5d7ebd8b508996f13e26dae8a528202cd175c24a
unknown <brightokusi@gmail.com>
2024-03-14 21:07:08 +0000

    Connecting to redshift and creating a schema for persisting the data tables

--- index.py
+++ index.py
@@ -8,7 +8,7 @@ import requests
 import psycopg2
 
 from utils.help import create_bucket, retrieve_api_data_and_upload_to_s3, transform_and_upload_to_s3
-from utils.constants import raw_file_path, transformed_file_path
+from utils.constants import raw_file_path, transformed_file_path, dev_schema
 
 config = configparser.ConfigParser()
 config.read('.env')
@@ -25,6 +25,12 @@ role = config['AWS']['role']
 ['API']
 api_key = config['API']['api_key']
 
+['DWH']
+dwh_host = config['DWH']['host']
+dwh_user = config['DWH']['user']
+dwh_password = config['DWH']['password']
+dwh_database = config['DWH']['database']
+
 
 #=====create an s3 bucket
 create_bucket(access_key = access_key, secret_key = secret_key, bucket_name = raw_bucket_name, region = region)
@@ -40,3 +46,17 @@ create_bucket(access_key=access_key, secret_key=secret_key, bucket_name=transfor
 
 #========== copy data from raw_data_bucket, transform and load to transformed_data_bucket
 transform_and_upload_to_s3(access_key, secret_key, raw_bucket_name, raw_file_path, transformed_bucket_name, transformed_file_path)
+
+
+#============ Create connection to data warehouse using redshift_connector
+conn_details = {'host':dwh_host, 'database': dwh_database, 'user':dwh_user, 'password': dwh_password}
+dwh_conn = rdc.connect(**conn_details)
+cursor = dwh_conn.cursor()
+print('connection successful')
+
+
+#============= Create a schema for the transformed jobs in Redshift warehouse
+query  = f'CREATE SCHEMA IF NOT EXISTS {dev_schema}'
+cursor.execute(query)
+dwh_conn.commit()
+print('created')
