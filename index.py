0f2dba7de7f6c188e0877a1accbd892bb0db3f04
unknown <brightokusi@gmail.com>
2024-03-14 20:57:20 +0000

    Creating the bucket for persisting transformed data

--- index.py
+++ index.py
@@ -27,9 +27,12 @@ api_key = config['API']['api_key']
 
 
 #=====create an s3 bucket
-create_bucket(access_key, secret_key, raw_bucket_name, region)
+create_bucket(access_key = access_key, secret_key = secret_key, bucket_name = raw_bucket_name, region = region)
 
 
 #========= Load json file to bucket
 retrieve_api_data_and_upload_to_s3(access_key, secret_key, api_key, raw_bucket_name, raw_file_path)
 
+
+#========== Create new bucket for transformed data
+create_bucket(access_key=access_key, secret_key=secret_key, bucket_name=transformed_bucket_name, region=region)
